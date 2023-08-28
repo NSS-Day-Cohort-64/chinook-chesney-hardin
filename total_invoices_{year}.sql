b0a7cd06cc21d745feb86a5f191658c83a1c560a
Chesney Hardin <chesney.hardin@gmail.com>
2023-08-28 11:55:48 -0500

    added separate quereis for 2009 and 2011

--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -4,3 +4,10 @@ SELECT COUNT(i.InvoiceId) AS InvoiceCountFor2009And2011
 FROM Invoice i 
 WHERE i.InvoiceDate LIKE "%2009%" OR i.InvoiceDate LIKE "%2011%"
 
+SELECT COUNT(i.InvoiceId) AS InvoiceCountFor2009
+FROM Invoice i 
+WHERE i.InvoiceDate LIKE "%2009%"
+
+SELECT COUNT(i.InvoiceId) AS InvoiceCountFor2011
+FROM Invoice i 
+WHERE i.InvoiceDate LIKE "%2011%"
